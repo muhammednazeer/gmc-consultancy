078651d52ed5ab2ed50e9c021894438ef242df28
Muhammed Hussaini Nazeer <53971194+muhammednazeer@users.noreply.github.com>
2026-07-23 06:31:02 +0100

    Remove unused template assets and libraries

--- js/main.js
+++ js/main.js
@@ -98,26 +98,4 @@
     time: 1000
   });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function () {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
-    $('#portfolio-flters li').on( 'click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-  
-      portfolioIsotope.isotope({ filter: $(this).data('filter') });
-    });
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
 })(jQuery);
-
